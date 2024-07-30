81fda32798bc0b131decd9a9a81bad12cfe8eba2
Arfelo Reed Torralba <CPAMaster1994@gmail.com>
2024-07-30 23:43:34 +0800

    edited setup for luasnip

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -4,14 +4,13 @@ return {
   dependencies = {
     "hrsh7th/cmp-buffer", -- source for text in buffer
     "hrsh7th/cmp-path", -- source for file system paths
-    -- {
-    --   "L3MON4D3/LuaSnip",
-    --   -- follow latest release.
-    --   version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-    --   -- install jsregexp (optional!).
-    --   build = "make install_jsregexp",
-    -- },
-    "L3MON4D3/LuaSnip",
+    {
+      "L3MON4D3/LuaSnip",
+      -- follow latest release.
+      version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+      -- install jsregexp (optional!).
+      build = "make install_jsregexp",
+    },
     "saadparwaiz1/cmp_luasnip", -- for autocompletion
     "rafamadriz/friendly-snippets", -- useful snippets
     -- "onsails/lspkind.nvim", -- vs-code like pictograms
